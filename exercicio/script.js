d92152ff6d4dec30cd2ab45a2420ed1faae8e5d8
Aline Viana <alinemaciel018@gmail.com>
2021-10-22 14:28:38 -0300

    do exerc with Singer Liniker

--- exercicio/script.js
+++ exercicio/script.js
@@ -1,35 +1,25 @@
-const json = [
-  {
-    "nome": "Liniker",
-    "foto": "../assets/liniker.webp",
-    "generos": ["MPB", "Soul", "R&B", "Blues"],
-    "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
-    "instagram": "https://www.instagram.com/linikeroficial/"
-  },
-  {
-    "nome": "Liniker",
-    "foto": "../assets/liniker.webp",
-    "generos": ["MPB", "Soul", "R&B", "Blues"],
-    "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
-    "instagram": "https://www.instagram.com/linikeroficial/"
-  },
-  {
-    "nome": "Liniker",
-    "foto": "../assets/liniker.webp",
-    "generos": ["MPB", "Soul", "R&B", "Blues"],
-    "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
-    "instagram": "https://www.instagram.com/linikeroficial/"
-  }
-]
+const objetoJson = {
+  "nome": "Liniker",
+  "foto": "../assets/liniker.webp",
+  "generos": ["MPB", "Soul", "R&B", "Blues"],
+  "discografia": ["Remonta", "Goela Abaixo", "Indigo Borboleta Azul"], 
+  "instagram": "https://www.instagram.com/linikeroficial/"
+}
 
-/* 
+// Suponha que nosso projeto consiste em um site de informações sobre cantoras brasileiras e na página em questão a usuária selecionou a cantora Liniker, e a API nos retornou um JSON com todas as informações que precisamos (armazenado na variável objetoJson acima), então devemos popular nosso HTML com essas informações utilizando JavaScript 
 
-O exercício consiste em usar javascript para popular com os dados do JSON armazenado na 'varável json' acima o mesmo site de informações sobre cantoras brasileiras que trabalhamos em aula, desta vez a página exibe 3 cantoras, em 3 cards diferentes em uma só página (preencha o JSON com dados das suas cantoras brasileiras preferidas)
+const photoOfSinger = document.getElementById('photography')
+const nameOfSinger = document.getElementById('title')
+const genderOfSinger = document.getElementById('gender')
+const discographyOfSinger = document.getElementById('discography')
+const instagramOfSinger = document.getElementById('instagram')
 
-Importante:
+function dataOfSinger() {
+  photoOfSinger.setAttribute('src', objetoJson.foto);
+  nameOfSinger.innerText = objetoJson.nome;
+  genderOfSinger.innerText = objetoJson.generos.join(" - ")
+  discographyOfSinger.innerText = objetoJson.discografia.join(" - ")
+  instagramOfSinger.setAttribute('href', objetoJson.instagram)
+}
 
-1. Crie seu próprio layout usando HTML e CSS nos arquivos já criados nesta pasta;
-2. Repare que este Json é uma matriz, logo é possível usar métodos de array para acessar seu conteúdo;
-3. Entregue este exercício da maneira que conseguir, use o exemplo da sala para guiar nesta construção;
-
-*/
+dataOfSinger()
